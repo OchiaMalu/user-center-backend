3c77ba45774324844b11013fea08d119a69c536c
ochiamalu <linzhehao1108@126.com>
2023-07-29 17:43:10 +0800

    feat: first init

---
+++ src/main/java/com/ochiamalu/user/UserCenterBackendApplication.java
@@ -0,0 +1,19 @@
+package com.ochiamalu.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 用户中心后端应用程序
+ *
+ * @author OchiaMalu
+ * @date 2023/07/29
+ */
+@SpringBootApplication
+public class UserCenterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterBackendApplication.class, args);
+    }
+
+}
